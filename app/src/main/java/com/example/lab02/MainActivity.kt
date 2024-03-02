c05fe3d8071874edfb9d4f9ad2d8e8b6dc245a2b
Abdirahman-ai <62496069+Abdirahman-ai@users.noreply.github.com>
2024-03-02 14:25:43 -0600

    strech feature upto step 8

--- app/src/main/java/com/example/lab02/MainActivity.kt
+++ app/src/main/java/com/example/lab02/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.lab02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.lab02.EmailFetcher.Companion.getNext5Emails
 
 class MainActivity : AppCompatActivity() {
     lateinit var emailList: List<Email>
@@ -19,5 +21,11 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter;
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        val loadMore = findViewById<Button>(R.id.loadMore);
+        loadMore.setOnClickListener {
+            val next5Emails = EmailFetcher.getNext5Emails();
+            emailList += next5Emails;
+            adapter.notifyDataSetChanged();
+        }
     }
 }
